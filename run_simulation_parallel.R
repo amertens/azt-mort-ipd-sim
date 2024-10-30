a3c39f6cf96692a38c37c4029c17da1008fc91c9
Andrew Mertens <amertens@berkeley.edu>
2024-10-29 20:46:30 -0700

    update sim

---
+++ run_simulation_parallel.R
@@ -0,0 +1,111 @@
+
+
+# Load required packages
+rm(list=ls())
+library(tidyverse)
+library(survival)
+library(lme4)
+library(here)
+library(metafor)
+library(foreach)
+library(parallel)
+library(doParallel)
+source("simulation_functions.R")
+source("study_params.R")
+
+
+# simulation parameters
+full_res = NULL
+n_analyses = 7  # Main + 4 age groups + 2 sex groups
+iter_df =NULL
+iter_range=c(7,1000)
+meta_method="REML"
+
+sim=1
+n_cores=50
+
+
+cl <- makeCluster(n_cores)
+clusterExport(cl, c("dataset_nums","run_targets_ltmle_simulation2",  "library",
+                    "SL.Control","null_sim",
+                    "n_bootstrap_samples",
+                    "Markov_variables",
+                    "gbounds",
+                    "n_df",
+                    "time","tmle_var"), envir=environment())
+
+clusterEvalQ(cl,lapply(c("fst","lava","ltmle","data.table","tidyverse","glmnet","Matrix","matrixStats","speedglm","parallel","caret","foreach","clustermq"), FUN = function(X) {
+  do.call("require", list(X)) 
+}))
+
+
+res=parLapply(cl=cl,dataset_nums, function(z) run_targets_ltmle_simulation2(dataset_num=z,
+                                                                            library=library,
+                                                                            null_sim=null_sim,
+                                                                            SL.Control=SL.Control,
+                                                                            n_bootstrap_samples=n_bootstrap_samples,
+                                                                            Markov_variables=Markov_variables,
+                                                                            gbounds=gbounds,
+                                                                            n=n_df,
+                                                                            time=time,
+                                                                            tmle_var=tmle_var))
+stopCluster(cl)
+
+
+
+
+
+
+# Run simulations
+for(sim in iter_range[1]:iter_range[2]){
+  
+  set.seed(sim)
+  # Generate data for all studies
+  sim_study_data <- do.call(rbind, lapply(names(study_params), function(study_name) {
+    params <- study_params[[study_name]]
+    simulate_trial_data(
+      n_clusters = params$n_clusters,
+      cluster_size = params$cluster_size, 
+      baseline_rate = params$baseline_rate,
+      effect_size = params$effect_size,
+      cv = params$cv,
+      study_name = study_name
+    )
+  }))
+  
+  # Run meta-analyses
+  results_list  <- meta_analyze_subgroups(sim_study_data, method=meta_method)
+  meta_results <- results_list[[1]]
+  study_results <- results_list[[2]]
+  
+  pooled_res <- NULL
+  for(j in seq_along(meta_results)) {
+    if(!is.null(meta_results[[j]])) {
+      res <- data.frame(
+        effect = exp(meta_results[[j]]$b[1]),
+        se = sqrt(meta_results[[j]]$vb[1,1]),
+        tau2 = meta_results[[j]]$tau2,
+        pvalue = meta_results[[j]]$pval
+      )
+      res$group <- names(meta_results)[j]
+    }
+    pooled_res <- bind_rows(pooled_res, res)
+  }
+  pooled_res$study="pooled"
+  
+  res=bind_rows(pooled_res, study_results)
+  res$iteration = sim
+  
+  full_res=bind_rows(full_res, res)
+  
+  # Save and print progress
+  saveRDS(full_res, file=here("results/sim_results_interim.rds"))
+  if(sim %% 10 == 0) cat(sprintf("Completed simulation %d of %d\n", sim, iter_range[2]))
+}
+
+
+
+saveRDS(full_res, file=here("results/sim_results.rds"))
+
+
+
