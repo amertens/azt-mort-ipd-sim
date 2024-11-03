3124f2ac04171e252eeaf4ac0e261cfde5ce7e16
Andrew Mertens <amertens@berkeley.edu>
2024-11-03 09:15:16 -0800

    run par

--- run_simulation_parallel.R
+++ run_simulation_parallel.R
@@ -1,5 +1,4 @@
 
-
 # Load required packages
 rm(list=ls())
 library(tidyverse)
@@ -8,56 +7,26 @@ library(lme4)
 library(here)
 library(metafor)
 library(foreach)
-library(parallel)
 library(doParallel)
+library(sandwich)
 source("simulation_functions.R")
+source("sim_data_functions.R")
 source("study_params.R")
+
 
 
 # simulation parameters
 full_res = NULL
-n_analyses = 7  # Main + 4 age groups + 2 sex groups
 iter_df =NULL
-iter_range=c(7,1000)
-meta_method="REML"
+iter_range=c(1,1000)
 
 sim=1
 n_cores=50
 
 
-cl <- makeCluster(n_cores)
-clusterExport(cl, c("dataset_nums","run_targets_ltmle_simulation2",  "library",
-                    "SL.Control","null_sim",
-                    "n_bootstrap_samples",
-                    "Markov_variables",
-                    "gbounds",
-                    "n_df",
-                    "time","tmle_var"), envir=environment())
-
-clusterEvalQ(cl,lapply(c("fst","lava","ltmle","data.table","tidyverse","glmnet","Matrix","matrixStats","speedglm","parallel","caret","foreach","clustermq"), FUN = function(X) {
-  do.call("require", list(X)) 
-}))
-
-
-res=parLapply(cl=cl,dataset_nums, function(z) run_targets_ltmle_simulation2(dataset_num=z,
-                                                                            library=library,
-                                                                            null_sim=null_sim,
-                                                                            SL.Control=SL.Control,
-                                                                            n_bootstrap_samples=n_bootstrap_samples,
-                                                                            Markov_variables=Markov_variables,
-                                                                            gbounds=gbounds,
-                                                                            n=n_df,
-                                                                            time=time,
-                                                                            tmle_var=tmle_var))
-stopCluster(cl)
-
-
-
-
-
-
-# Run simulations
-for(sim in iter_range[1]:iter_range[2]){
+run_sim_par <- function(full_res = NULL,
+                        sim =NULL,
+                        adjusted=FALSE, run_tmle=FALSE){
   
   set.seed(sim)
   # Generate data for all studies
@@ -72,40 +41,61 @@ for(sim in iter_range[1]:iter_range[2]){
       study_name = study_name
     )
   }))
+
+  # Run analyses
+  results <- run_analysis(studies_data=sim_study_data, adjusted=adjusted, run_tmle=run_tmle)
   
-  # Run meta-analyses
-  results_list  <- meta_analyze_subgroups(sim_study_data, method=meta_method)
-  meta_results <- results_list[[1]]
-  study_results <- results_list[[2]]
+  results$iteration = sim
   
-  pooled_res <- NULL
-  for(j in seq_along(meta_results)) {
-    if(!is.null(meta_results[[j]])) {
-      res <- data.frame(
-        effect = exp(meta_results[[j]]$b[1]),
-        se = sqrt(meta_results[[j]]$vb[1,1]),
-        tau2 = meta_results[[j]]$tau2,
-        pvalue = meta_results[[j]]$pval
-      )
-      res$group <- names(meta_results)[j]
-    }
-    pooled_res <- bind_rows(pooled_res, res)
-  }
-  pooled_res$study="pooled"
+  full_res=bind_rows(full_res, results)
   
-  res=bind_rows(pooled_res, study_results)
-  res$iteration = sim
-  
-  full_res=bind_rows(full_res, res)
-  
-  # Save and print progress
-  saveRDS(full_res, file=here("results/sim_results_interim.rds"))
-  if(sim %% 10 == 0) cat(sprintf("Completed simulation %d of %d\n", sim, iter_range[2]))
+  return(full_res)
 }
 
 
+cl <- makeCluster(n_cores)
+clusterExport(cl, c("run_sim_par","study_params","simulate_trial_data","generate_covariates",
+                    "run_analysis","analyze_subgroups","analyze_trial_data","iter_range"), envir=environment())
 
-saveRDS(full_res, file=here("results/sim_results.rds"))
+
+clusterEvalQ(cl,lapply(c("tmle","data.table","tidyverse","survival","lme4","sandwich","here"), FUN = function(X) {
+  do.call("require", list(X))
+}))
+
+# temp = run_sim_par(full_res = NULL,
+#             sim =1,
+#             adjusted=FALSE,
+#             run_tmle=FALSE)
+# temp
+
+full_res=NULL
+#for(i in iter_range[1]:(iter_range[2]/n_cores)){
+for(i in 3:20){
+  cat(i,"\n")
+  res=parLapply(cl=cl, c(((i-1)*50+1):(50*i)), function(z) run_sim_par(full_res = NULL,
+                                                        sim =z,
+                                                        adjusted=TRUE, 
+                                                        run_tmle=TRUE))
+  resdf=data.table::rbindlist(res)
+  full_res=bind_rows(full_res, resdf)
+  saveRDS(full_res, file=here("results/sim_results_interim_par.rds"))
+}
+
+
+saveRDS(full_res, file=here("results/sim_results_par.rds"))
+stopCluster(cl)
+
+
+
+
+
+# simulation parameters
+full_res = NULL
+iter_df =NULL
+iter_range=c(1,1000)
+
+sim=1
+
 
 
 
