cf71b818372aceff962cf0e4a614499a6889b41f
Andrew Mertens <amertens@berkeley.edu>
2024-11-07 06:02:11 -0800

    update performance calculation

--- calc_performance.R
+++ calc_performance.R
@@ -12,78 +12,134 @@ source("sim_data_functions.R")
 source("study_params.R")
 
 truth <- readRDS(here("results/truth.rds"))
+study_results <- readRDS(file=here("results/sim_results_par.rds"))
 
-sim_results_full <- readRDS(file=here("results/sim_results_interim.rds"))
-sim_results_pooled <- sim_results_full %>% filter(study=="pooled")
+#NOTE! Need to add in unadjusted results
 
-study_results <- sim_results_full %>% filter(study!="pooled")
-#calculate Pvalue from effect and se
-study_results$pvalue <- 2 * (1 - pnorm(abs(study_results$effect/ study_results$se)))
+colnames(study_results)
 
-d <- left_join(study_results, truth, by=c("study", "group"))
+#transform data to long format with columns for effect and se, and the group
+study_result_est <- study_results %>% 
+  mutate(crude_rr = log(crude_rr)) %>% #make sure all ratios are on the log scale
+  select(iteration, study, group, level, crude_rr, hr, cid, cir, tmle_log_rr, tmle_ate) %>%
+  gather(key="metric", value="effect", -iteration, -study, -group, -level) %>% distinct()
 
-unique(study_results$group)
-unique(truth$group)
+study_result_se <- study_results %>% 
+  select(iteration, study, group, level, hr_se, cid_se, cir_se, tmle_rr_log_se, tmle_ate_se) %>%
+  gather(key="metric", value="se", -iteration, -study, -group, -level) %>%
+  mutate(metric = gsub("_se", "", metric)) %>% distinct()
 
-d <- d %>% rename(estimate=effect, std.err=se, trueRD=IR_diff, trueRR=IR_ratio) %>%
-  mutate(lower=estimate-1.96*std.err, upper=estimate+1.96*std.err)
+study_result_pval <- study_results %>% 
+  select(iteration, study, group, level, hr_pval,  cir_pval, cid_pval, tmle_rr_pval, tmle_ate_pval) %>%
+  gather(key="metric", value="pval", -iteration, -study, -group, -level) %>%
+  mutate(metric = gsub("_pval", "", metric)) %>% distinct()
 
-head(d)
+study_results <- left_join(study_result_est, study_result_se, by=c("iteration", "study", "group", "level", "metric"))
+study_results <- left_join(study_results, study_result_pval, by=c("iteration", "study", "group", "level", "metric"))
+study_results <- study_results %>% mutate(lower=effect-1.96*se, upper=effect+1.96*se)
 
+#join with truth
+table(study_results$metric)
+table(truth$metric)
+
+truth <- truth %>% mutate(true_value = case_when(
+  metric %in% c("cid", "ird") ~ true_value,
+  metric %in% c("cir", "irr") ~ log(true_value)
+)) %>% filter(!is.na(metric))
+truth
+
+truth_tmle <- truth %>% mutate(metric=case_when(
+  metric=="cid" ~ "tmle_ate",
+  metric=="cir" ~ "tmle_log_rr"
+)) %>% filter(!is.na(metric))
+truth <- bind_rows(truth, truth_tmle)
+
+table(study_results$study)
+table(study_results$group)
+table(study_results$level)
+table(study_results$metric)
+table(truth$study)
+table(truth$group)
+table(truth$metric)
+
+d <- left_join(study_results, truth, by=c("study", "level", "metric"))
+table(d$metric,!is.na(d$true_value))
+table(d$level,!is.na(d$true_value))
 # Calculate performance metrics
+tail(d)
 
-#Need to add RD to estimation
-# resRD <- d %>% 
-#     summarise(abs_bias_RD=mean(abs(estimate-trueRD)),
-#               estimator_variance_RD=mean(((estimate)-mean((estimate)))^2),
-#               mean_variance_RD=mean((std.err)^2),
-#               bias_se_ratio_RD=abs_bias_RD/sqrt(mean_variance_RD),
-#               coverage_RD=mean(lower<=trueRD & trueRD<=upper)*100,
-#               O_coverage_RD=mean(estimate-1.96*sd(estimate)< trueRD & trueRD < estimate+1.96*sd(estimate))*100
-#     )
+summary( d$effect[d$metric=="tmle_log_rr"] )
+truth
 
+res <- d %>% group_by(study,level, metric) %>%
+  summarise(n=n(),
+            true_value=true_value[1],
+            abs_bias=mean(abs(effect - true_value)),
+            estimator_variance=mean(((effect )-mean((effect )))^2),
+            mean_variance=mean((se)^2),
+            bias_se_ratio=abs_bias/sqrt(mean_variance),
+            coverage=mean(lower<=true_value & true_value<=upper)*100,
+            O_coverage=mean(effect -1.96*sd(effect )< true_value & true_value < effect +1.96*sd(effect ))*100)
+head(res)
 
-  
-resRR <- d %>% group_by(group) %>%
-    summarise(est_RR=exp(mean(estimate)),
-              trueRR=exp(mean(log(trueRR))),
-              abs_log_bias_RR=mean(abs(estimate-log(trueRR))),
-              estimator_variance_RR=mean(((estimate)-mean((estimate)))^2),
-              mean_variance_RR=mean((std.err)^2),
-              bias_se_ratio_RR=abs_log_bias_RR/sqrt(mean_variance_RR),
-              power = mean(pvalue<0.05) * 100,
-              coverage_RR=mean(lower<=log(trueRR) & log(trueRR)<=(upper))*100,
-              O_coverage_RR=mean((estimate)-1.96*sd((estimate))< log(trueRR) & 
-                                   log(trueRR) < (estimate)+1.96*sd((estimate)))*100)
-resRR
+#temp drop HR
+res <- res %>% filter(metric!="hr", metric!="crude_rr")
 
-resRR_study <- d %>% group_by(group, study) %>%
-  summarise(est_RR=exp(mean(estimate)),
-            trueRR=exp(mean(log(trueRR))),
-            abs_log_bias_RR=mean(abs(estimate-log(trueRR))),
-            estimator_variance_RR=mean(((estimate)-mean((estimate)))^2),
-            mean_variance_RR=mean((std.err)^2),
-            power = mean(pvalue<0.05) * 100,
-            bias_se_ratio_RR=abs_log_bias_RR/sqrt(mean_variance_RR),
-            coverage_RR=mean(lower<=log(trueRR) & log(trueRR)<=(upper))*100,
-            O_coverage_RR=mean((estimate)-1.96*sd((estimate))< log(trueRR) & 
-                                 log(trueRR) < (estimate)+1.96*sd((estimate)))*100)
-resRR_study
+#-------------------------------------------------------------------------------
+# get meta-analysis performance
+#-------------------------------------------------------------------------------
+
+#XXXXXXXXXXXXXXXXXXX
+#Add
+
 
 #-------------------------------------------------------------------------------
 # plot_performance
 #-------------------------------------------------------------------------------
 
 #transform data to long format
-resRR_study_long <- resRR_study %>% 
-  select(group, study,  abs_log_bias_RR, estimator_variance_RR, 
-         power, bias_se_ratio_RR, coverage_RR, O_coverage_RR ) %>%
-  gather(key="metric", value="value", -group, -study)
+resRR_study_long <- res %>% 
+  select(level, metric, study,  abs_bias, estimator_variance, mean_variance, bias_se_ratio, coverage, O_coverage ) %>%
+  gather(key="performance_metric", value="value", -level, -metric,, -study)
 
-ggplot(resRR_study_long, aes(x=study, y=value, color=group, shape=group)) +
+ggplot(resRR_study_long, aes(x=study, y=value, color=metric  , shape=metric  )) +
   coord_flip() +
   geom_point() +
-  facet_wrap(~metric, scales="free") +
+  facet_wrap(~performance_metric, scales="free") +
+  theme_minimal()
+
+unique(resRR_study_long$level)
+resRR_study_long<-resRR_study_long %>% mutate(level=factor(level, levels=c("24-59mo",  "12-23mo", "6-11mo" ,"1-5mo", "female","male","main" )))
+
+temp=resRR_study_long %>% filter(performance_metric=="O_coverage")
+ggplot(resRR_study_long %>% filter(performance_metric=="O_coverage"), aes(x=level, y=value, color=metric  , shape=metric  )) +
+  coord_flip() +
+  geom_hline(yintercept = 95, linetype="dashed") +
+  geom_point(position=position_dodge(0.5)) +
+  facet_wrap(~study, scales="fixed") +
+  theme_minimal()
+
+ggplot(resRR_study_long %>% filter(performance_metric=="coverage"), aes(x=level, y=value, color=metric  , shape=metric  )) +
+  coord_flip() +
+  geom_hline(yintercept = 95, linetype="dashed") +
+  geom_point(position=position_dodge(0.5)) +
+  facet_wrap(~study, scales="fixed") +
+  theme_minimal()
+
+
+ggplot(resRR_study_long %>% filter(performance_metric=="abs_bias", metric %in% c("cid","tmle_ate")), 
+       aes(x=level, y=value, color=metric  , shape=metric  )) +
+  coord_flip() +
+  geom_hline(yintercept = 0, linetype="dashed") +
+  geom_point(position=position_dodge(0.5)) +
+  facet_wrap(~study, scales="fixed") +
+  theme_minimal()
+
+ggplot(resRR_study_long %>% filter(performance_metric=="bias_se_ratio", metric %in% c("cir","tmle_log_rr")),
+       aes(x=level, y=value, color=metric  , shape=metric  )) +
+  coord_flip() +
+  geom_point(position=position_dodge(0.5)) +
+  facet_wrap(~study, scales="fixed") +
   theme_minimal()
   
 #tab = merge(resRD, resRR, by=c("Estimator","estimator"))
--- calc_truth.R
+++ calc_truth.R
@@ -62,49 +62,73 @@ study_params$TANA_I$cluster_size <- 1000
     )
   }))
   
-  table(sim_study_data_A1$deaths)
-  table(sim_study_data_A0$deaths)
+  # table(sim_study_data_A1$dead)
+  # table(sim_study_data_A0$dead)
   
-  #---------------------------------------------------------------------------
-  # Calculate truth by study and subgroup
-  #---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+# Calculate truth by study and subgroup
+#---------------------------------------------------------------------------
+  
+  A1 = sim_study_data_A1 %>% mutate(level = "main")
+  A0 = sim_study_data_A0 %>% mutate(level = "main")
+  level_var = "level"
 
-calc_group_truth <- function(A1, A0, group_var) {
+calc_group_truth <- function(A1, A0, level_var) {
+  
+  head(A1)
     
-    truth_A1 <- A1 %>% group_by(study, !!sym(group_var)) %>%
+    truth_A1 <- A1 %>% group_by(study, !!sym(level_var)) %>%
       summarize(
-        incidence = mean(deaths),
+        incidence = mean(dead),
         person_time = sum(exp(log_offset)),
         IR=incidence/person_time * 1000000) %>%
-       rename(group = !!sym(group_var))
-    truth_A0 <- A0 %>% group_by(study, !!sym(group_var)) %>%
+       rename(level = !!sym(level_var))
+    
+    head(truth_A1)
+    
+    truth_A0 <- A0 %>% group_by(study, !!sym(level_var)) %>%
       summarize(
-        incidence = mean(deaths),
+        incidence = mean(dead),
         person_time = sum(exp(log_offset)),
         IR=incidence/person_time * 1000000)%>%
-      rename(group = !!sym(group_var))
+      rename(level = !!sym(level_var))
     
-    truth <- left_join(truth_A1, truth_A0,  by=c("study","group")) %>%
+    truth <- left_join(truth_A1, truth_A0,  by=c("study","level")) %>%
       mutate(
-        IR_diff = IR.x - IR.y,
-        IR_ratio = IR.x / IR.y
+        cid= incidence.x - incidence.y,
+        cir= incidence.x / incidence.y,
+        ird = IR.x - IR.y,
+        irr = IR.x / IR.y
       ) %>%
-      select(study, IR_diff, IR_ratio, group) 
+      select(study, cid,  cir, ird, irr, level)
     
+
+
+    # # HR calculation: Ratio of event rates
+    # delta1 <- sum(A1$dead) / sum(A1$time)
+    # delta0 <- sum(A0$dead) / sum(A0$time)
+    # true_hr<- delta1/delta0
+    # 
     return(truth)
 }
   
-  truth_main = calc_group_truth(sim_study_data_A1 %>% mutate(group = "main"), 
-                                sim_study_data_A0 %>% mutate(group = "main"), "group") 
-  truth_age = calc_group_truth(sim_study_data_A1, sim_study_data_A0, "age_group") %>% mutate(group=paste0("age_", group), group=factor(group, levels=c("age_1-5mo", "age_6-11mo", "age_12-23mo", "age_24-59mo"))) %>% arrange(group) 
-  truth_sex = calc_group_truth(sim_study_data_A1, sim_study_data_A0, "sex") %>% mutate(group=case_when(group==1 ~ "sex_male",group==0 ~ "sex_female"))
+  truth_main = calc_group_truth(sim_study_data_A1 %>% mutate(level = "main"), 
+                                sim_study_data_A0 %>% mutate(level = "main"), "level") 
+  truth_age = calc_group_truth(sim_study_data_A1, sim_study_data_A0, "age_group") %>% mutate(level=factor(level, levels=c("1-5mo", "6-11mo", "12-23mo", "24-59mo"))) %>% arrange(level) 
+  truth_sex = calc_group_truth(sim_study_data_A1, sim_study_data_A0, "sex") %>% mutate(level=case_when(level==1 ~ "male",level==0 ~ "female"))
 
   
   #combine
   truth <- bind_rows(truth_main, truth_age, truth_sex)
+  
+  #transform for merge with results
+  truth <- truth %>% pivot_longer(cols=c(cid, cir, ird, irr), names_to="metric", values_to="true_value")
+  table(truth$metric)
+  
   saveRDS(truth, file=here("results/truth.rds"))
   
   
   truth_main %>% filter(study=="MORDOR_Niger") 
   truth_age %>% filter(study=="MORDOR_Niger") 
+  
   
